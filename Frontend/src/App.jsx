be40161922484a23bba17d59fa64c92e7e33f1f6
Abdullah44 <130264823+abdullli@users.noreply.github.com>
2024-04-29 19:46:20 +0600

    Frontend Done!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,22 @@
+
+import Home from "./home/Home"
+import { Route, Routes } from "react-router-dom"
+import Courses from "./courses/Courses"
+import Singup from "./components/Singup"
+
+
+const App = () => {
+  return (
+    <>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Course" element={<Courses />} />
+          <Route path="/singup" element={<Singup />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/Freebook.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import Slider from "react-slick"
+
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+import List from "../../public/list.json"
+import Cards from "./Cards"
+
+const Freebook = () => {
+  const filterData = List.filter((data) => data.category === "Free")
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
+
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="text-xl font-semibold pb-2">Free Offered Courses</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing alit. Accusantium veritais alias palias pariatur ad dolor requdiandae eligendi  corporis nulla non suscipit, iure neque earun?
+          </p>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            {
+              filterData.map((item)=>(
+                <Cards item={item} key={item.id} />
+              ))
+            }
+          </Slider>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Freebook
